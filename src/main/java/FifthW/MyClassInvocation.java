c9d0e1e8ef6aeeccd08d55b6367ddac4d3b24501
kirill shcherba <kirillshcherba92@gmail.com>
2021-09-02 00:22:15 +0300

    Second

--- src/main/java/FifthW/MyClassInvocation.java
+++ src/main/java/FifthW/MyClassInvocation.java
@@ -12,7 +12,7 @@ public class MyClassInvocation implements InvocationHandler {
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        System.out.println("Будет подсчёт");
+        System.out.println("Будет подсчёт моих данных");
         return null;
     }
 }
